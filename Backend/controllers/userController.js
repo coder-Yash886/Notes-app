1b1dc8c31f855862a9bc9b4b33fb9e78868357d7
coder-Yash886 <yashkumar.967565@gmail.com>
2026-03-24 09:17:40 +0530

    Add the forgotpassword functionality

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -200,3 +200,37 @@ export const registerUser = async (req,res) =>{
     }
 
  }
+
+ export const forgotPassword = async (req,res) =>{
+    try{
+
+        const {email} = req.body;
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "Invalid email "
+            })
+        }
+
+        const otp = Math.floor(100000 + Math.random()*9000000).toString();
+        const expiry = new Date(Date.now() + 10*60*1000);
+
+        user.otp = otp;
+        user.otpExpiry = expiry;
+        await user.save();
+
+        await sendOtpMail(email, otp);
+        return res.status(200).json({
+            success: false,
+            message: error.message
+        })
+
+    } catch(error){
+        return res.status(500).json({  
+            success: false,
+            message: error.message
+        })
+    }
+ }
